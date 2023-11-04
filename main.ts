246bd490b0a240dad9a3a634ee89ec0f812ce950
miyazaki <108732709+miyazaki-tsubasa@users.noreply.github.com>
2023-11-04 16:15:23 +0900

    質問数を10個に追加

--- main.ts
+++ main.ts
@@ -15,19 +15,51 @@ function handleFormSubmit(event) {
         q1: form.q1.value,
         q2: form.q2.value,
         q3: form.q3.value,
+        q4: form.q1.value,
+        q5: form.q2.value,
+        q6: form.q3.value,
+        q7: form.q1.value,
+        q8: form.q2.value,
+        q9: form.q3.value,
+        q10: form.q1.value,
     };
     displayResults(answers);
 }
 
-function displayResults(answers) {
-    // const results = `あなたの回答は以下の通りです:\n質問1: ${answers.q1}\n質問2: ${answers.q2}\n質問3: ${answers.q3}`;
-    // 質問で選択されたラジオボタンのラベルテキストを取得
-    const q1Label = document.querySelector(`label[for="${answers.q1}"]`).textContent;
-    const q2Label = document.querySelector(`label[for="${answers.q2}"]`).textContent;
-    const q3Label = document.querySelector(`label[for="${answers.q3}"]`).textContent;
-
-    // 回答結果を表示する文字列を作成
-    const results = `あなたの回答は以下の通りです<br>質問1: ${q1Label}<br>質問2: ${q2Label}<br>質問3: ${q3Label}`;
+// ラベルのテキストを取得する関数
+function getLabelText(questionNumber: string, answerValue: string): string {
+    return document.querySelector(`label[for="${answerValue}"]`).textContent;
+}
+    
+function displayResults(answers) {  
+  // 全ての回答ラベルを取得
+  const labels = [
+    getLabelText('q1', answers.q1),
+    getLabelText('q2', answers.q2),
+    getLabelText('q3', answers.q3),
+    getLabelText('q4', answers.q4),
+    getLabelText('q5', answers.q5),
+    getLabelText('q6', answers.q6),
+    getLabelText('q7', answers.q7),
+    getLabelText('q8', answers.q8),
+    getLabelText('q9', answers.q9),
+    getLabelText('q10', answers.q10)
+  ];
+  
+  // 回答結果を表示する文字列をテンプレートリテラルで作成
+  const results = `
+    あなたの回答は以下の通りです<br>
+    質問1: ${labels[0]}<br>
+    質問2: ${labels[1]}<br>
+    質問3: ${labels[2]}<br>
+    質問4: ${labels[3]}<br>
+    質問5: ${labels[4]}<br>
+    質問6: ${labels[5]}<br>
+    質問7: ${labels[6]}<br>
+    質問8: ${labels[7]}<br>
+    質問9: ${labels[8]}<br>
+    質問10: ${labels[9]}
+  `;
  
     // 結果を表示する前にフォームを非表示にする
     const form = document.getElementById('psych-test');
