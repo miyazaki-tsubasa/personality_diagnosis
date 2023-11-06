95dd6e2040f56f2ba9e97b72ac2af7231966b412
miyazaki <108732709+miyazaki-tsubasa@users.noreply.github.com>
2023-11-06 11:56:31 +0900

    質問回答に対して、適切な診断結果を表示させる機能を追加

--- main.ts
+++ main.ts
@@ -30,60 +30,60 @@ function handleFormSubmit(event) {
 
     // 警告メッセージをクリア
     document.getElementById('warning-message').innerHTML = '';
+    console.log('form.q1.value : ', form.q1.value)
+    console.log('form.q2.value : ', form.q2.value)
+    console.log('form.q3.value : ', form.q3.value)
+    console.log('form.q4.value : ', form.q4.value)
+    console.log('form.q5.value : ', form.q5.value)
+    console.log('form.q6.value : ', form.q6.value)
+    console.log('form.q7.value : ', form.q7.value)
+    console.log('form.q8.value : ', form.q8.value)
+    console.log('form.q9.value : ', form.q9.value)
+    console.log('form.q10.value : ', form.q10.value)
 
     const answers = {
-        q1: form.q1.value,
-        q2: form.q2.value,
-        q3: form.q3.value,
-        q4: form.q1.value,
-        q5: form.q2.value,
-        q6: form.q3.value,
-        q7: form.q1.value,
-        q8: form.q2.value,
-        q9: form.q3.value,
-        q10: form.q1.value,
+        q1: form.q1.value === 'morning' ? 'Aの選択肢' : 'Bの選択肢',
+        q2: form.q2.value === 'planner' ? 'Aの選択肢' : 'Bの選択肢',
+        q3: form.q3.value === 'thinker' ? 'Aの選択肢' : 'Bの選択肢',
+        q4: form.q4.value === 'leader' ? 'Aの選択肢' : 'Bの選択肢',
+        q5: form.q5.value === 'adaptable' ? 'Aの選択肢' : 'Bの選択肢',
+        q6: form.q6.value === 'detailed' ? 'Aの選択肢' : 'Bの選択肢',
+        q7: form.q7.value === 'logical' ? 'Aの選択肢' : 'Bの選択肢',
+        q8: form.q8.value === 'listener' ? 'Aの選択肢' : 'Bの選択肢',
+        q9: form.q9.value === 'careful' ? 'Aの選択肢' : 'Bの選択肢',
+        q10: form.q10.value === 'practical' ? 'Aの選択肢' : 'Bの選択肢',
     };
-    displayResults(answers);
+      // 警告メッセージがなければ結果を表示
+    if (document.getElementById('warning-message').innerHTML === '') {
+        const personalityType = determinePersonalityType(answers);
+        displayResults(personalityType); // この関数は性格タイプを表示するように更新する必要がある
+    }
+    // displayResults(answers);
 }
 
-// ラベルのテキストを取得する関数
-function getLabelText(questionNumber: string, answerValue: string): string {
-    return document.querySelector(`label[for="${answerValue}"]`).textContent;
+// ユーザーが選択したラジオボタンの値に基づいてラベルのテキストを取得するために使用
+function getLabelText(answerValue: string): string {
+    const labelElement = document.querySelector(`label[for="${answerValue}"]`);
+    if (labelElement) {
+      return labelElement.textContent;
+    } else {
+      return '未選択'; // ラベルが見つからない場合はこのテキストを返す
+    }
 }
     
-function displayResults(answers) {  
-  // 全ての回答ラベルを取得
-  const labels = [
-    getLabelText('q1', answers.q1),
-    getLabelText('q2', answers.q2),
-    getLabelText('q3', answers.q3),
-    getLabelText('q4', answers.q4),
-    getLabelText('q5', answers.q5),
-    getLabelText('q6', answers.q6),
-    getLabelText('q7', answers.q7),
-    getLabelText('q8', answers.q8),
-    getLabelText('q9', answers.q9),
-    getLabelText('q10', answers.q10)
-  ];
-  
-  // 回答結果を表示する文字列をテンプレートリテラルで作成
-  const results = `
-    あなたの回答は以下の通りです<br>
-    質問1: ${labels[0]}<br>
-    質問2: ${labels[1]}<br>
-    質問3: ${labels[2]}<br>
-    質問4: ${labels[3]}<br>
-    質問5: ${labels[4]}<br>
-    質問6: ${labels[5]}<br>
-    質問7: ${labels[6]}<br>
-    質問8: ${labels[7]}<br>
-    質問9: ${labels[8]}<br>
-    質問10: ${labels[9]}
-  `;
+function displayResults(personalityType) {  
  
     // 結果を表示する前にフォームを非表示にする
     const form = document.getElementById('psych-test');
     form.style.display = 'none'; // フォームを非表示にする
+
+    // 診断結果を表示する文字列をテンプレートリテラルで作成
+    const results = `
+    <h2>診断結果</h2>
+    あなたの性格タイプは「${personalityType}」です。<br>
+    以下にあなたの回答に基づいた性格の特徴をいくつか紹介します：<br>
+    <!-- ここに性格タイプに応じた説明を追加 -->
+    `;
 
     // HTMLドキュメント内のIDが 'message' の要素を取得
     // HTMLコンテンツを results 文字列に置き換え
@@ -92,6 +92,36 @@ function displayResults(answers) {
     messageElement.innerHTML = results;
     // document.getElementById('message').innerHTML = results;
 }
+
+// 回答に基づいて性格タイプを決定する関数
+function determinePersonalityType(answers: Record<string, string>): string {
+    let typeACount = 0;
+    let typeBCount = 0;
+  
+    // ここで「A」または「B」をカウントするためのロジックを追加
+    for (const key of Object.keys(answers)) {
+      if (answers[key] === 'Aの選択肢') {
+        typeACount++;
+      } else {
+        typeBCount++;
+      }
+    }
+    console.log("typeACount : ", typeACount)
+    console.log("typeBCount : ", typeBCount)
+
+    // ここでカウントに基づいて性格タイプを決定
+    if (typeACount >= 10) {
+      return '思考タイプ';
+    } else if (typeACount >= 8) {
+      return '感情タイプ';
+    } else if (typeACount >= 6) {
+      return 'バランスタイプ';
+    } else if (typeACount >= 4) {
+      return '直感タイプ';
+    } else if (typeACount <= 3){
+      return '感覚タイプ';
+    }
+}  
 
 document.addEventListener('DOMContentLoaded', () => {
     // 歓迎メッセージを表示
