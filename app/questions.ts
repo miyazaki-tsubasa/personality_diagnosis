a61ff08d1469ac80df13e6c90721f987445a7b81
miyazaki <108732709+miyazaki-tsubasa@users.noreply.github.com>
2023-11-09 17:29:17 +0900

    htmlでのコードを最低限にしてtsで管理

---
+++ app/questions.ts
@@ -0,0 +1,13 @@
+export const questions = [
+    { id: 'q1', text: '新しい経験を積極的に求めますか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q2', text: '日々のタスクに対して計画的ですか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q3', text: '社交的なイベントが好きですか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q4', text: '他人の感情に共感しやすいですか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q5', text: '小さな問題でも簡単に心配になりますか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q6', text: '芸術作品や音楽を深く理解することができますか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q7', text: 'タスクを完了するために余分な時間を費やすことが多いですか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q8', text: '新しい人と出会うのが楽しみですか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q9', text: '人の助けを借りずに問題を解決することが多いですか？', options: ['yes', 'neither', 'no'] },
+    { id: 'q10', text: '将来に対して不安を感じることがよくありますか？', options: ['yes', 'neither', 'no'] }
+  ];
+  
